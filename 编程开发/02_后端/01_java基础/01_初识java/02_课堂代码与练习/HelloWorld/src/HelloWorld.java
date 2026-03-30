96be653cfd2737644989cfb0553c34e5844f2e61
锏澹 <1446300258@qq.com>
2026-03-30 22:55:36 +0800

    复习整理 初识java笔记 重构初识java笔记结构 截图全部本地化 新增模板文件夹 目录模板 笔记模板 错题模板: 2026-03-30 22:54:46

---
+++ 编程开发/02_后端/01_java基础/01_初识java/02_课堂代码与练习/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * ClassName: HelloWorld
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author:锏澹
+ * @Create: 2026/3/30 - 22:40
+ * @Version: v1.0
+ *
+ */
+public class HelloWorld {
+    //main方法 程序的主入口
+    public static void main(String[] args) {
+        /*
+        打印 语句 输出（）内的内容
+        */
+        System.out.println("HelloWorld 你好啊世界");
+    }
+}
